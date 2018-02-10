5045cb18131c24edbf47e37fddb430bd499eb970
vyshnavijyothermai <vyshnavi.jyothermaisri@gmail.com>
2018-02-10 10:29:45 +0530

    mgg

--- src/reg/reg.controller.js
+++ src/reg/reg.controller.js
@@ -4,12 +4,14 @@ class RegController{
        this.name="";
        this.email="";    
        this.password="";
+       this.cpassword="";
     }
 doReg()
     {
       console.log(this.name);
       console.log(this.email);
       console.log(this.password);
+      console.log(this.cpassword);
     }
 }
 export default RegController;
